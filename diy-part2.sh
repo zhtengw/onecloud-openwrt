95889ed78a05c892615c2bf6ae5d38612d794f89
zhtengw <atenzd@gmail.com>
2025-12-27 16:17:27 +0800

    Default IP change to 192.168.22.11

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.10.99/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.22.11/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
